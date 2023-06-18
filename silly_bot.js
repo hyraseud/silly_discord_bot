fe4252678d19e2f373f05777c7d1b65444deee15
Shreya D <54275890+hyraseud@users.noreply.github.com>
2023-06-18 18:19:45 -0400

    Create silly_bot.js
    hehe

---
+++ silly_bot.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+let lastNumber = 0; // Initialize the last sent number
+let notCount = 0;
+
+// Event handler for bot startup
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+// Event handler for incoming messages
+client.on('message', (message) => {
+  // Ignore messages from the bot itself
+  if (message.author.bot) return;
+
+  // Check if the message is in the "counting" channel
+  if (message.channel.name === 'counting・') {
+    // Parse the content as a number
+    const number = parseInt(message.content);
+
+    // Check if the content is a valid number and greater than the last sent number
+    if (/^\d+$/.test(message.content)) {
+      // Update the last sent number
+      if (number === lastNumber + 1) {
+      lastNumber = number;
+
+      notCount = 0;
+      }
+      else {
+         message.channel.send(`YOU SUCK AT COUNTING BOOOOOO GO BACK TO SCHOOL`);
+      }
+    } else {
+    
+      notCount++;
+      if (notCount === 10) {
+        
+          message.channel.send(`Wow, it's been so long since you've counted...`);
+        
+        }
+    }
+  }
+});
