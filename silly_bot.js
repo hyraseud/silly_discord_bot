c3c4a28331217b1575ff90349a6199d0a7ecc3e9
Shreya D <54275890+hyraseud@users.noreply.github.com>
2023-06-29 12:54:15 -0400

    Update silly_bot.js
    fixes

--- silly_bot.js
+++ silly_bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-let lastNumber = 8190; // Initialize the last sent number
+let lastNumber = 0; // Initialize the last sent number
 let notCount = 0;
 
 // Event handler for bot startup
@@ -16,29 +16,37 @@ client.on('message', (message) => {
 
   // Check if the message is in the "counting" channel
   if (message.channel.name === 'counting・') {
-    // Parse the content as a number
-  
     // Check if the content is a valid number and greater than the last sent number
     if (/^\d+$/.test(message.content)) {
       notCount = 0;
       // Update the last sent number
       const number = parseInt(message.content);
-      if (number === lastNumber + 1) {
+      if (number === lastNumber + 1 || lastNumber === 0) {
       lastNumber = number;
       notCount = 0;
       }
-      else if (lastNumber != 8190 && number != lastNumber + 1) {
+      else if (lastNumber != 0 && number != lastNumber + 1) {
          message.channel.send(`YOU SUCK AT COUNTING BOOOOOO GO BACK TO SCHOOL :bell:`);
          lastNumber = 0; //back to 0 we go
       }
-    } else {
-    
-      notCount++;
-      if (notCount === 10) {
-        
-          message.channel.send(`Wow, it's been so long since you've counted...`);
-        
-        }
-    }
   }
+  else if (message.content.toLowerCase().includes('good morning') || message.content.toLowerCase().includes('gm')) {
+    notCount++;
+    message.channel.send(`Good morning ${message.member.displayName}!`);
+  }  
+
+  
+  else if (message.content.toLowerCase().includes('goodnight') || message.content.toLowerCase().includes('good night') || message.content.toLowerCase().includes('gn')) {
+    notCount++;
+    message.channel.send(`Goodnight ${message.member.displayName}!`);
+  } 
+  else {
+    
+    notCount++;
+    if (notCount === 10) {
+        message.channel.send(`Wow, it's been so long since you've counted...`);
+      
+      }
+  }
+}
 });
